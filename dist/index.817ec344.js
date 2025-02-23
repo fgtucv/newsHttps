6c9783415efc53962c45ede12ddfa578cb306dc4
fgtucv <oles.slava23@gmail.com>
2025-02-23 15:52:27 +0200

    g

--- dist/index.817ec344.js
+++
@@ -1,7 +0,0 @@
-const e=document.querySelector(".news-list"),t=document.querySelector(".input");document.querySelector(".button").addEventListener("click",()=>{n().then(e=>{s(e.articles)})});const s=t=>{let s;e.innerHTML="",s="",s=t.map(e=>`<li class="news-list__item">
-            <h2 class="news-list__title">${e.author}</h2>
-            <p class="news-list__text">${e.content}</p>
-            <a href="" class="news-list__url">${e.url}</a>
-            <span class="news-list__date">${e.publishedAt}</span>
-        </li>`).join(""),e.innerHTML=s},n=()=>fetch(`https://newsapi.org/v2/everything?q=${t.value}&apiKey=7bab116baf184c4689a16929a05cfb49`).then(e=>e.json());
-//# sourceMappingURL=index.817ec344.js.map
